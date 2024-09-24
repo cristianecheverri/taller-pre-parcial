c6d76dfb8c99816094abccfae0edf068fbbfa5ae
Cristian Camilo Echeverri <crisechevrri@gmail.com>
2024-09-24 18:32:24 -0500

    Se agrega data a taller

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 const titulo = document.getElementById('titulo');
 titulo.innerHTML = 'Mi tienda';
 
-fetch("/data/taller.json")
+fetch("../../data/taller.json")
     .then(function(response) {return response.json()})
     .then((data) => {
         console.log(data);
